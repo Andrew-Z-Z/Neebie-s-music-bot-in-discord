b6b2903c6cda2c1af8d75278a12c9b9d7590928b
Andrew Zhou <andersonzhoutree@gmail.com>
2020-07-03 23:57:22 -0700

    added commands, client folder for performance optimization added Client.js to give special properties to child class of Discord.Client classin index.js, removed if/else clause for command execution, replaced with try/catch created play.js that exports an object containing execute function for index.js done

--- commands/play.js
+++ commands/play.js
@@ -1,9 +1,83 @@
 const ytdl = require('ytdl-core');
+const { getInfo } = require('ytdl-getinfo');
 
 module.exports = {
   name: "play",
   description: "Play the Music in the Queue",
-  async execute(message) {
-    console.log(message)
+  async execute(message, args) {
+    try {
+      const queue = message.client.queue;
+      const serverQueue = queue.get(message.guild.id);
+
+      const voiceChannel = message.member.voice.channel;
+      if (!voiceChannel || voiceChannel.name !== "Bot-Music") {
+        message.reply("You must be in Bot-Music voice Channel to Listen to Music")
+        return;
+      }
+
+      /*
+        I used a module called "ytdl-getInfo" to make ytdl availble to take in a string, search on Youtube, and returns an unknown video
+      */
+      const song = await getInfo(args.join(" ")).then(info => {
+        return {
+          title: info.items[0].title,
+          url: info.items[0].webpage_url
+        }
+      });
+
+      if (!serverQueue) {
+        const queueStructure = {
+          textChannel: message.channel,
+          voiceChannel: voiceChannel,
+          connection: null,
+          songs: [],
+          volume: 5,
+          playing: true
+        };
+
+        queue.set(message.guild.id, queueStructure);
+
+        queueStructure.songs.push(song)
+
+        try {
+          let connection = await voiceChannel.join();
+          queueStructure.connection = connection;
+          this.play(message, queueStructure.songs[0])
+        } catch (error) {
+          console.error(error);
+          queue.delete(message.guild.id);
+          return message.channel.send("error");
+        }
+      } else {
+        serverQueue.songs.push(song);
+        return message.channel.send(`${song.title} has been added to the Queue!`);
+      }
+    } catch(error) {
+      console.error(error);
+      message.channel.send("Outside Error")
+    }
+  },
+  play(message, song) {
+    const queue = message.client.queue;
+    const guild = message.guild;
+    const serverQueue = queue.get(guild.id);
+
+    if (!song) {
+      serverQueue.voiceChannel.leave();
+      queue.delete(guild.id);
+      return;
+    }
+
+    const dispatcher = serverQueue.connection
+    .play(ytdl(song.url, {type: 'opus', filter: 'audioonly'}))
+    .on('finish', () => {
+      serverQueue.songs.shift();
+      this.play(message, serverQueue.songs[0])
+    })
+    .on("error", (err) => {
+      console.error(err);
+    });
+    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+    serverQueue.textChannel.send(`Start playing --${song.title}`);
   }
 }
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config');
-const ytdl = require('ytdl-core');
+const Client = require('./client/Client');
 
-const client = new Discord.Client();
+const client = new Client();
 client.commands = new Discord.Collection();
 
 /*
@@ -18,89 +18,31 @@ for (const file of commandFiles) {
   const command = require(`./commands/${file}`)
   client.commands.set(command.name, command)
 }
-console.log(client.commands);
+
+client.queue = new Map();
 
 client.on('ready', () => {
   console.log(`Bot ${client.user.tag} is ready!`)
 });
 
 client.on('message', async msg => {
+  if (msg.author.bot || !msg.content.startsWith(prefix)) return;
+  if (msg.channel.name !== "bot-order") {
+    msg.channel.send('You Can\'t Command the Bot in current Channel!')
+    return;
+  }
   const args = msg.content.substring(prefix.length).split(/ +/);
   const commandName = args.shift().toLowerCase();
   const command = client.commands.get(commandName);
-  console.log(args);
 
-  if (msg.author.bot || !msg.content.startsWith(prefix)) return;
-  if (msg.channel.name !== "bot-order") {
-    msg.channel.send('You Can\'t Command the Bot in this Channel!')
-    return;
+  if (!client.commands.has(commandName)) return;
+
+  try {
+    command.execute(msg, args);
+  } catch(error) {
+    console.error(error);
+    msg.reply(`There is an unexpected err: ${error}`)
   }
-
-})
-
-
-// async function executeMusic(msg, serverQueue) {
-//   let args = msg.content.substring(prefix.length).split(" ");
-//   const voiceChannel = msg.member.voice.channel;
-
-//   if (!voiceChannel || voiceChannel.name !== 'Bot-Music') {
-//     msg.channel.send("You must be in \'Bot-Music\' Channel to listen to music")
-//   }
-//   if (!args[1]) {
-//     msg.channel.send("Please Provide a Valid Youtube VideoLink")
-//   }
-
-//   const songInfo = await ytdl.getInfo(args[1]);
-//   const song = {
-//     title: songInfo.title,
-//     url: songInfo.video_url
-//   };
-
-//   if (!serverQueue) {
-//     const queueStructure = {
-//       textChannel: msg.channel,
-//       voiceChannel: voiceChannel,
-//       connection: null,
-//       songs: [],
-//       volume: 5,
-//       playing: true
-//     }
-
-//     queue.set(msg.guild.id, queueStructure);
-//     queueStructure.songs.push(song);
-
-//     try {
-//       let connection = await voiceChannel.join();
-//       queueStructure.connection = connection;
-//       playMusic(msg.guild.id, queueStructure.songs[0]);
-//     } catch (err) {
-//       console.error(err);
-//       queue.delete(msg.guild.id);
-//       return msg.channel.send(err);
-//     }
-//   } else {
-//     serverQueue.songs.push(song);
-//     return msg.channel.send(`${song.title} has been added to the queue!`);
-//   }
-// }
-
-// function playMusic(guild, song) {
-//   const serverQueue = queue.get(guild);
-//   if (!song) {
-//     serverQueue.voiceChannel.leave();
-//     queue.delete(guild.id);
-//     return;
-//   }
-
-//   const dispatcher = serverQueue.connection
-//   .play(ytdl(song.url, {filter: "audioonly", /* highWaterMark: 1024 * 1024 * 24 */}))
-//   .on("finish", () => {
-//     serverQueue.songs.shift();
-//     playMusic(guild, serverQueue.songs[0])
-//   })
-//   .on("error", err => { console.error(error) });
-//   dispatcher.setVolumeLogarithmic(serverQueue.volume / 5)
-//   serverQueue.textChannel.send(`Start playing ${song.title}`)
-// }
+});
 
 client.login(token);
